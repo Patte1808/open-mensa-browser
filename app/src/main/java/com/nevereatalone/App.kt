4c45b08b1335c530d312a1ccf40d53ca05da7c50
chunyuan <cagehouhou@gmail.com>
2018-03-20 23:00:00 +0100

    Setup CI and CD config

--- app/src/main/java/com/nevereatalone/App.kt
+++ app/src/main/java/com/nevereatalone/App.kt
@@ -5,12 +5,15 @@ import com.nevereatalone.data.api.ApiModule
 import timber.log.BuildConfig
 import timber.log.Timber
 import javax.inject.Inject
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
 
 class App : Application() {
     @Inject
-    lateinit var app : Application
+    lateinit var app: Application
 
-     val component: AppComponent by lazy {
+    val component: AppComponent by lazy {
         DaggerAppComponent.builder()
                 .appModule(AppModule(this.applicationContext))
                 .apiModule(ApiModule())
@@ -20,7 +23,13 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initTimber()
+        initFabric()
         component.inject(this)
+    }
+
+    private fun initFabric() {
+        if (!com.nevereatalone.BuildConfig.DEBUG)
+            Fabric.with(this, Crashlytics())
     }
 
     private fun initTimber() {
